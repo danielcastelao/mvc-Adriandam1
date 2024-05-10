496ec0d27670e45fb169f84782329361ac4a7c16
Adrian <aabeijoncarbajo@danielcastelao.org>
2024-05-10 10:07:42 +0200

    creado view

--- src/cod/mvc/Controller.java
+++ src/cod/mvc/Controller.java
@@ -3,5 +3,6 @@ package cod.mvc;
 public class Controller {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 }
---
+++ src/cod/mvc/View.java
@@ -0,0 +1,9 @@
+package cod.mvc;
+
+public class View {
+    static boolean muestraVelocidad(String matricula, int velocidad){
+        System.out.println("Matricula; "+matricula+" va  a "+velocidad+" km/h");
+        return true;
+    }
+
+}
